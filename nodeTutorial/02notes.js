a6a8ca5990533b3149c975e2bbb4c479f15d820f
Amruth <ammumandappa@gmail.com>
2024-07-06 18:57:37 +0530

    .

---
+++ nodeTutorial/02notes.js
@@ -0,0 +1,8 @@
+console.log(`notes page is loaded  `);
+
+
+var age = 22;
+
+module.exports = {
+    age
+}
--- nodeTutorial/server.js
+++ nodeTutorial/server.js
@@ -1,20 +1,31 @@
-var fs = require("fs");
-var os = require("os");
+// var fs = require("fs");
+// var os = require("os");
 
-var user = os.userInfo();
-console.log(user.username);
-console.log(user.name);
+// var user = os.userInfo();
+// console.log(user.username);
+// console.log(user.name);
 
-fs.appendFile("greeting.txt", "hi" + user.username + "!\n", () => {
-  console.log("File is created");
-});
+// fs.appendFile("greeting.txt", "hi" + user.username + "!\n", () => {
+//   console.log("File is created");
+// });
 
-// fs.appendFile(file Path, data, callback function)
-fs.appendFile("newFile.txt", "hi " + user.username + "\n", () => {
-  console.log("new file created");
-});
+// // fs.appendFile(file Path, data, callback function)
+// fs.appendFile("newFile.txt", "hi " + user.username + "\n", () => {
+//   console.log("new file created");
+// });
 
-console.log(os)
-console.log("\n")
-console.log(fs)
- 
+// console.log(os)
+// console.log("\n")
+// console.log(fs)
+
+const notes = require(`./02notes.js`)
+var _ = require('loadash')
+console.log("hiii")
+const age = notes.age;
+console.log(age)
+
+// var data = ['person1', "person1", 'person2', 1, 2, 3, 2, 8,8]
+// var filter = _.unique(data)
+
+// console.log(filter)
+  
