9b4753080703feefd00a1ac8c8235e7cd05e6f79
Amruth <ammumandappa@gmail.com>
2024-07-07 19:37:51 +0530

    .

--- nodeTutorial/express.js
+++ nodeTutorial/express.js
@@ -9,4 +9,16 @@ app.get('/chicken', (req, res)=> {
     res.send('1 plate chicken')
 })
 
-app.listen(3000)
+app.get('/idli', (req, res)=> {
+    var customized_idli = {
+        name: 'rava idli',
+        size: "10 cm in diameter",
+        is_sambar: true,
+        quantity: 2,
+    }
+    res.send( customized_idli)
+})
+
+app.listen(3000, ()=> {
+    console.log("listening to port 3000")
+}) 
