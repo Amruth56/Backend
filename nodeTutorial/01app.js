51d8ef9c643ae5d360a44fa748c553a66e5fa821
Amruth <ammumandappa@gmail.com>
2024-07-09 18:42:44 +0530

    .

--- nodeTutorial/01app.js
+++ nodeTutorial/01app.js
@@ -1,3 +1,8 @@
-var cnt = require('./06moduleNrequire');
+// var cnt = require('./06moduleNrequire');
 
-console.log(cnt(['hi', 'hello', 'namaste']))
+// console.log(cnt(['hi', 'hello', 'namaste']))
+
+var mathfn = require('./07modulePatterns')
+console.log(mathfn.multiply(2, 3))
+console.log(mathfn.add(2, 3))
+console.log(mathfn.substract(2, 3))
---
+++ nodeTutorial/07modulePatterns.js
@@ -0,0 +1,13 @@
+ var adder = function(a, b){
+    return `sum of ${a} and ${b} is ${a+b}`;
+ }
+ var sub = function(a, b){
+    return `Difference of ${a} and ${b} is ${a-b}`;
+ }
+ var mul = function(a, b){
+    return `multiplication of ${a} and ${b} is ${a*b}`;
+ }
+
+ module.exports.add = adder
+ module.exports.substract = sub
+ module.exports.multiply = mul
