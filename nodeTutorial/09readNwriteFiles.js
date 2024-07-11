9719b241e661e9bad7f5a0ec567b5ced42512611
Amruth <ammumandappa@gmail.com>
2024-07-11 12:32:37 +0530

    read and write the file asynchronously

--- nodeTutorial/09readNwriteFiles.js
+++ nodeTutorial/09readNwriteFiles.js
@@ -1,12 +1,32 @@
-//  to read and write files using fs
+// //  to read and write files using fs synchronous way
 
-var fs = require("fs");
+// var fs = require("fs");
 
-var readMe = fs.readFileSync("readme.txt", "utf8");
+// var readMe = fs.readFileSync("readme.txt", "utf8");
 
+// // to write file
+// // (where to write, what to write)
+// fs.writeFileSync("writeMe.txt", readMe);
 
-// to write file 
-// (where to write, what to write)
-fs.writeFileSync("writeMe.txt", readMe);
+// console.log(readMe);
 
-console.log(readMe);
+// ------------------------------------------------------------------------------------------
+
+//  to read and write files using fs Asynchronous way
+
+const fs = require("fs");
+
+// Asynchronous approach
+fs.readFile("readme.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  fs.writeFile("asyncWriteMe.txt", data, (writeErr) => {
+    if (writeErr) {
+      console.error("Error writing file:", writeErr);
+      return;
+    }
+    console.log("File written successfully!");
+  });
+});
