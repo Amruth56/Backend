36aa5e3240db4bfdd8b0401269670b888b46ca4d
Amruth <ammumandappa@gmail.com>
2024-07-17 16:17:47 +0530

    .

--- nodeTutorial/17express.js
+++ nodeTutorial/17express.js
@@ -12,4 +12,10 @@ app.get('/contact', (req, res)=> {
 app.get('/login', (req, res)=> {
     res.send('this is the login page')
 })
+app.get('/profile/:id', (req, res)=> {
+    res.send('this is the profile page ' + req.params.id)
+})
+app.get('/character/:name', (req, res)=> {
+    res.send('the name of the person is :' + req.params.name)
+})
 app.listen(3000)
