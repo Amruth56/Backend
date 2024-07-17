3e5507a79ce9e2ddeb5470dc65dc48058f2e04bd
Amruth <ammumandappa@gmail.com>
2024-07-17 19:04:21 +0530

    .

--- nodeTutorial/18templateEngines.js
+++ nodeTutorial/18templateEngines.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/14index.html')
@@ -8,6 +9,10 @@ app.get('/', (req, res)=> {
 
 app.get('/contact', (req, res)=> {
     res.sendFile(__dirname + '/16contact.html')
+})
+
+app.get('/profile/:name',  (req, res)=> {
+    res.render('profile', {person: req.params.name })
 })
 
 app.listen(3000)
