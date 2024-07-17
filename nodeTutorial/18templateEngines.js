16e1e8910547a8a880e855057b4d8395abcd9a2c
Amruth <ammumandappa@gmail.com>
2024-07-17 20:08:48 +0530

    .

--- nodeTutorial/18templateEngines.js
+++ nodeTutorial/18templateEngines.js
@@ -1,19 +1,19 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.get('/', (req, res)=> {
-    res.sendFile(__dirname + '/14index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/14index.html");
+});
 
-app.get('/contact', (req, res)=> {
-    res.sendFile(__dirname + '/16contact.html')
-})
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/16contact.html");
+});
 
-app.get('/profile/:name',  (req, res)=> {
-    var data1 = {age: 29, job: 'Software Developer'}
-    res.render('profile', {person: req.params.name, data: data1 })
-})
+app.get("/profile/:name", (req, res) => {
+  var data1 = { age: 29, job: "Software Developer", hobbies: ["eating", 'fighting', 'fishing'] };
+  res.render("profile", { person: req.params.name, data: data1 });
+});
 
-app.listen(3000)
+app.listen(3000);
