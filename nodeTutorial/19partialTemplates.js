31dac952ca4a217b3c18a360f7c30d2ed018862b
Amruth <ammumandappa@gmail.com>
2024-07-18 10:14:56 +0530

    partial views

---
+++ nodeTutorial/19partialTemplates.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render('contact');
+});
+
+app.get("/profile/:name", (req, res) => {
+  var data1 = {
+    age: 29,
+    job: "Software Developer",
+    hobbies: ["eating", "fighting", "fishing"],
+  };
+  res.render("profile", { person: req.params.name, data: data1 });
+});
+
+app.listen(3000);
