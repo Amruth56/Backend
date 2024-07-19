dc3d58fc20ded9ef0964115625e252c933a29986
Amruth <ammumandappa@gmail.com>
2024-07-19 15:04:20 +0530

    controller

--- todoList/controllers/todoController.js
+++ todoList/controllers/todoController.js
@@ -1,3 +1,13 @@
 module.exports = function(app){
-    
+    app.get('/todo', function(req, res){
+
+    })
+
+    app.post('/todo', function(req, res){
+
+    })
+
+    app.delete('/todo', (req, res)=> {
+        
+    })
 }
