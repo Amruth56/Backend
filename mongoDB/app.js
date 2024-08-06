3ce64fdf95c8bcdcf3d6f0f03adba9f196ae3168
Amruth <ammumandappa@gmail.com>
2024-08-07 00:21:20 +0530

    create update read delete

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -4,11 +4,39 @@ const app = express();
 const userModel = require("./usermodel");
 
 app.get("/", (req, res) => {
-  res.send("hey its an index page  ");
+  res.send("hey its an index page");
 });
 
-app.get('/create', (req, res)=> {
-    res.send("hey")
-})
+app.get("/create", async (req, res) => {
+  let createdUser = await userModel.create({
+    name: "harshitha",
+    email: "harsh@gmail.com",
+    userName: "Harshitha005",
+    roll: 57,
+  });
+  res.send(createdUser);
+});
 
-app.listen(3000);
+app.get("/update", async (req, res) => {
+  let updateUser = await userModel.findOneAndUpdate(
+    { userName: "Harsh005" },
+    { name: "Harsh Rawat" },
+    { new: true }
+  );
+  res.send(updateUser);
+});
+ 
+app.get('/read', async (req, res)=> {
+   let users = await   userModel.find();
+   res.send(users);
+
+});
+
+app.get('/delete', async (req, res)=> {
+    let delUser = await userModel.findOneAndDelete({userName:"Harshitha005"});
+    res.send(delUser);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
--- mongoDB/usermodel.js
+++ mongoDB/usermodel.js
@@ -9,4 +9,4 @@ const userSchema = mongoose.Schema({
 
 })
 
-module.exports = mongoose.model("user", userSchema)
+module.exports =  mongoose.model("user", userSchema)
