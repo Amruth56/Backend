2e1c0e7e3705f0c55167d11f9a91cd6e1e00cf2b
Amruth <ammumandappa@gmail.com>
2024-08-07 20:57:59 +0530

    understanding cookies

---
+++ authentication/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.cookie("name", "qwerty");
+  res.send("done");
+});
+app.get("/read", (req, res) => {
+  console.log(req.cookies);
+  res.send("read page");
+});
+
+app.listen(3000);
