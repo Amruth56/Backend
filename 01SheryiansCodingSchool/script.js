71284d5d0be706c38553620c45979280487151a8
Amruth <ammumandappa@gmail.com>
2025-01-15 07:33:03 +0530

    .

--- 01SheryiansCodingSchool/script.js
+++ 01SheryiansCodingSchool/script.js
@@ -1,1 +1,12 @@
-alert("")
+// alert("Hello World");
+
+async function abcd() {
+    
+    var blob = await fetch(`https://randomuser.me/api/`);
+    var ans = await blob.json();
+    
+    console.log(ans);
+    console.log(ans.results);
+}
+
+abcd();
