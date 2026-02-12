cdef29e95378ddd627e18ff773bd8f0d5972dbbe
Amruth <ammumandappa@gmail.com>
2026-02-12 11:54:09 +0530

    diaplayed the names in the ui

--- Django/employees/views.py
+++ Django/employees/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from employees.models import Employee
-from django.http import Http404
+from django.http import Http404, HttpResponse
 
 # Create your views here.
 
 def employee_detail(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     print(employee)
+    return HttpResponse(employee)
     # try:
     #     employee = Employee.objects.get(pk = pk)
     #     print(employee)
