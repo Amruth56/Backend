2ccc538300e1d90b74430f2345cb8fc1e945e634
Amruth <ammumandappa@gmail.com>
2026-02-12 11:52:48 +0530

    rendered data from http response

--- Django/employees/views.py
+++ Django/employees/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from employees.models import Employee
+from django.http import Http404
 
 # Create your views here.
+
+def employee_detail(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    print(employee)
+    # try:
+    #     employee = Employee.objects.get(pk = pk)
+    #     print(employee)
+    # except:
+    #     raise Http404("Employee not found")
