c698a4709c0933fcc105cdc162c670254c3be25f
Amruth <ammumandappa@gmail.com>
2026-02-12 09:04:06 +0530

    createed an emoloyee app and its model

---
+++ Django/employees/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    photo = models.ImageField(upload_to='images')
+    designation = models.CharField(max_length = 100)
+    email_address = models.EmailField(unique=True, max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+   
+    
